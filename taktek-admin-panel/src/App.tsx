47858a92f0166d4602cc9bdb551274510169beba
caszofficial <santiagozapata07@gmail.com>
2025-01-23 14:37:16 -0500

    removed auth0 landing

--- taktek-admin-panel/src/App.tsx
+++ taktek-admin-panel/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PersistentDrawer from "./components/PersistentDrawer";
 import Users from "./pages/Users";
 import Services from "./pages/Services";
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import EditUsers from "./pages/Edit/EditUsers";
 import CreateUser from "./pages/Create/CreateUser";
 import EditServices from "./pages/Edit/EditServices";
@@ -13,64 +13,42 @@ import CreateService from "./pages/Create/CreateService";
 import Companies from "./pages/Companies";
 import EditCompany from "./pages/Edit/EditCompany";
 import EditTechnician from "./pages/Edit/EditTechnician";
-import { useAuth0 } from "@auth0/auth0-react";
-import Login from "./pages/Login";
 
 function App() {
   const drawerWidth = 240;
-  const { isAuthenticated, isLoading } = useAuth0();
-
-  console.log(isAuthenticated);
-
-  if (isLoading) {
-    return (
-      <Box>
-        <CircularProgress />
-      </Box>
-    );
-  }
 
   return (
     <>
-      {!isAuthenticated ? (
-        <Box>
+      <Box>
+        <ToastContainer />
+        <PersistentDrawer />
+        <Box
+          sx={{
+            width: `calc(100% - ${drawerWidth}px)`,
+            margin: "auto",
+            minHeight: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/create" element={<CreateUser />} />
+            <Route path="/users/:id" element={<EditUsers />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/create" element={<CreateService />} />
+            <Route path="/services/:id" element={<EditServices />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route
+              path="/companies/:companyId/technicians/:technicianId"
+              element={<EditTechnician />}
+            />
+            <Route path="/companies/:id" element={<EditCompany />} />
           </Routes>
         </Box>
-      ) : (
-        <Box>
-          <ToastContainer />
-          <PersistentDrawer />
-          <Box
-            sx={{
-              width: `calc(100% - ${drawerWidth}px)`,
-              margin: "auto",
-              minHeight: "100vh",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Routes>
-              <Route path="/home" element={<Home />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/users/create" element={<CreateUser />} />
-              <Route path="/users/:id" element={<EditUsers />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/services/create" element={<CreateService />} />
-              <Route path="/services/:id" element={<EditServices />} />
-              <Route path="/companies" element={<Companies />} />
-              <Route
-                path="/companies/:companyId/technicians/:technicianId"
-                element={<EditTechnician />}
-              />
-              <Route path="/companies/:id" element={<EditCompany />} />
-            </Routes>
-          </Box>
-        </Box>
-      )}
+      </Box>
     </>
   );
 }
--- taktek-admin-panel/src/main.tsx
+++ taktek-admin-panel/src/main.tsx
@@ -3,20 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Auth0Provider
-      domain="taktek.us.auth0.com"
-      clientId="qZlYsv8vZyVMRp5nlBUpTAvyh30d1gfB"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
